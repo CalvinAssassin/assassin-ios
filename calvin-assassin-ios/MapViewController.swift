ac5e3c9a88dd655aa6258c7ca9da5ac697481958
Christiaan Hazlett <christiaanhazlett@gmail.com>
2016-11-15 01:23:11 -0500

    Initial commit to repo; created project and added views for screens in the app, linked together with a tab bar UI element.

---
+++ calvin-assassin-ios/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  calvin-assassin-ios
+//
+//  Created by Christiaan Hazlett on 11/12/16.
+//  Copyright © 2016 CS262B. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Map";
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ calvin-assassin-ios/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  calvin-assassin-ios
+//
+//  Created by Christiaan Hazlett on 11/12/16.
+//  Copyright © 2016 CS262B. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
